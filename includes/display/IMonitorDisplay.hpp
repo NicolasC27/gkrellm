d38171a51af0ea9fc8639645fd5b498aa69642be
valentin gerard <valentin.gerard@epitech.eu>
2017-01-21 14:34:54 +0100

    add architecture

---
+++ includes/display/IMonitorDisplay.hpp
@@ -0,0 +1,9 @@
+//
+// IMonitorDisplay.hpp for IMonitorDisplay in /home/gerard_v/CPP_Pool/cpp_gkrellm/includes/display
+// 
+// Made by valentin gerard
+// Login   <gerard_v@epitech.net>
+// 
+// Started on  Sat Jan 21 14:27:11 2017 valentin gerard
+// Last update Sat Jan 21 14:27:11 2017 valentin gerard
+//
---
+++ includes/modules/IMonitorModule.hpp
@@ -0,0 +1,10 @@
+//
+// IMonitorModule.hpp for IMonitorModule in /home/gerard_v/CPP_Pool/cpp_gkrellm/includes/modules
+// 
+// Made by valentin gerard
+// Login   <gerard_v@epitech.net>
+// 
+// Started on  Sat Jan 21 14:26:37 2017 valentin gerard
+// Last update Sat Jan 21 14:26:39 2017 valentin gerard
+//
+
