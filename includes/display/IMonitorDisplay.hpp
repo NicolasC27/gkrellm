23d7cb59385d80a1c58a33ed46ad6f18bcb0812e
valentin gerard <valentin.gerard@epitech.eu>
2017-01-21 15:10:46 +0100

    add Interfaces + add gitignore file for qt creator

--- includes/display/IMonitorDisplay.hpp
+++ includes/display/IMonitorDisplay.hpp
@@ -5,5 +5,25 @@
 // Login   <gerard_v@epitech.net>
 // 
 // Started on  Sat Jan 21 14:27:11 2017 valentin gerard
-// Last update Sat Jan 21 14:27:11 2017 valentin gerard
+// Last update Sat Jan 21 15:09:06 2017 valentin gerard
 //
+
+#ifndef __IMONITORDISPLAY_HPP_
+# define __IMONITORDISPLAY_HPP_
+
+enum DisplayType
+  {
+    QT,
+    NCURSES
+  };
+
+class IMonitorDisplay
+{
+public:
+  virtual		~IMonitorDisplay() {};
+  virtual DisplayType	getStatus() = 0;
+  virtual std::string	getDisplayName() = 0;
+  virtual int		size() = 0;
+};
+
+#endif /* !__IMONITORDISPLAY_HPP_ */
--- includes/modules/IMonitorModule.hpp
+++ includes/modules/IMonitorModule.hpp
@@ -5,6 +5,20 @@
 // Login   <gerard_v@epitech.net>
 // 
 // Started on  Sat Jan 21 14:26:37 2017 valentin gerard
-// Last update Sat Jan 21 14:26:39 2017 valentin gerard
+// Last update Sat Jan 21 14:49:22 2017 valentin gerard
 //
 
+#ifndef __IMONITORMODULE_HPP_
+# define __IMONITORMOFULE_HPP_
+
+# include <string>
+
+class IMonitorModule
+{
+public:
+  virtual		~IMonitorModule() {};
+  virtual bool		getStatus() = 0;
+  virtual std::string	getName() = 0;
+};
+
+#endif /* IMONITORMODULE_HPP_ */
